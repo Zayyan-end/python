7ac180d571066aa030255e3d5c99a39e20a0de8d
Zayyan <bintesakeenah3@gmail.com>
2025-05-25 11:22:00 +0100

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,19 @@
+print('Welcome to th world of programming!')
+print(4)
+print('Hello python\n')
+print('hello',100)
+print('Welcome to',end="*")
+
+x=21
+y='Jeff'
+print(x)
+print(y)
+codingal='codingal'
+print(codingal)
+
+name=input("Please Enter Your Name.")
+print('\n hello',name,'\n welcome to codingal')
+
+import keyword
+print('python keywords are...\n')
+print(keyword.kwlist)
