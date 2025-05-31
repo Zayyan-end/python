f185121ea16c68bf9b2709f794c9621c65e62edd
Zayyan <bintesakeenah3@gmail.com>
2025-05-31 09:58:49 +0100

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,14 @@
+a=5
+print('type of a: ',type(a))
+b=2.5
+print('type of b: ',type(b))
+c='hello'
+print('type of c: ',type(c))
+B=True
+print('type of B:' ,type(B))
+print('\n after type casting')
+a=str(a)
+print('type of a: ',type(a))
+b=int(b)
+print('type of b: ',type(b))
+print(b)
