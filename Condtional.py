cce21ed059020028296780637c05e487b9b7dd66
Zayyan <bintesakeenah3@gmail.com>
2025-06-07 10:12:29 +0100

    Add files via upload

---
+++ Condtional.py
@@ -0,0 +1,29 @@
+num = 34
+
+if num > 0:
+    print(num,'is a positive number')
+num = -1
+if num > 0:
+    print(num,'is a positive number')
+else:
+    print(num,'is a negitive number')
+
+
+pa=int(input('enter purchase amount'))
+sa=int(input('enter sale amount'))
+if sa > pa:
+    profit=sa-pa
+    print('total profit is:',profit)
+
+else:
+    print(' you have no profit.')
+
+
+i=int(input('enter a number'))
+if(i < 15):
+    print('i is smaller than 15')
+    print('im in if Block')
+else:
+    print('i is greater than 15')
+    print('im in else Block')
+print('im not in in if and not in else block')
