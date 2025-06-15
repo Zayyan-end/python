84e3ecc63a3ba334926fcd3835d454f656092851
Zayyan <bintesakeenah3@gmail.com>
2025-06-15 11:31:51 +0100

    Add files via upload

---
+++ operrators.py
@@ -0,0 +1,36 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+ans=(v+w)*x/y
+print('the aswers is', ans)
+
+name='alex'
+age=0
+if name == 'alex' or name == 'John' and age >= 2:
+    print('Hello welcome!')
+else:
+    print('good bye')
+
+
+print('Enter a number(Numerator): ')
+numn=int(input())
+print('Enter a number(Denomintor): ')
+numd=int(input())
+
+if numn%numd==0:
+    print('numbers are divisible by each other')
+else:
+    print('Number is not devisible by each other')
+
+
+mean1=38
+wn=36
+cn=56
+tn=40
+sum=mean1*tn
+print('the sum of 40 number:' ,sum)
+num2=sum-((wn)-(cn))
+mean2=num2/tn
+print('correct mean is:' , mean2)
