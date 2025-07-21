861f8f7fb56a5c57bcfe668bd3f5507556f7df6c
Zayyan <bintesakeenah3@gmail.com>
2025-07-21 12:07:04 +0100

    Add files via upload

---
+++ Alphabets.py
@@ -0,0 +1,5 @@
+ch=input('Enter any single character')
+if ((ch>='A' and ch<='Z')or(ch>='a' and ch<='z')):
+    print('given value is an alphabet')
+else:
+    print('given value is not an alphabet')
